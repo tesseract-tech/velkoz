cef67ddd2fd808d1e22e4f64d234912f2e639c84
Tim Smith <tim.smith@sparksgrove.com>
2016-03-05 22:29:44 -0500

    addes eune endpoint

--- lib/velkoz/region.ex
+++ lib/velkoz/region.ex
@@ -5,6 +5,8 @@ defmodule Velkoz.Region do
         case region do
            "na" ->
              "https://na.api.pvp.net"
+           "eune"->
+               "https://eune.api.pvp.net"
         end
 
     end
