d2c0efd4e278d107912a0cd17243f78a3c608cb7
Tim Smith <tsmith2686@gmail.com>
2016-03-11 01:11:18 -0500

    updated readme and information for hex

--- README.md
+++ README.md
@@ -2,6 +2,33 @@
 
 More information coming soon
 
+# Using Velkoz with mix
+
+```elixir
+def deps do
+[
+  {:velkoz, "~>0.0.1"}
+]
+end
+```
+
+#Configuration
+```bash
+export LOL_API_KEY='00000000000000000000000000000' #Get your api key from the leage website
+export LOL_TIME_FRAME='0000000000000000000000000000000' # default time from league should be 10000000
+export LOL_REQUEST_LIMIT='0000000000000000000000000' # default reqest limit should be 500
+```
+
+
+# Example usage
+
+```elixir
+Velkoz.Summoner.by_name("na","zerovapor")
+#%{"zerovapor" => %{"id" => 33739329, "name" => "zerovapor",
+#    "profileIconId" => 989, "revisionDate" => 1457495041000,
+#    "summonerLevel" => 30}}
+```
+
 
 ## Other API Libraries
 
--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule Velkoz.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description,
+     package: package,
      deps: deps]
   end
 
@@ -30,7 +32,25 @@ defmodule Velkoz.Mixfile do
     [
       {:httpoison, "~> 0.8.0"},
       {:ex_rated, "~> 1.2.1"},
-      {:poison, "~> 2.1"}
+      {:poison, "~> 2.1"},
+      {:earmark, "~> 0.1", only: :dev},
+      {:ex_doc, "~> 0.11", only: :dev}
   ]
   end
+
+  defp description do
+    """
+    Velkoz is an api wrapper for the game leauge of legends. You can find the api documentation :: https://developer.riotgames.com/api
+    """
+  end
+
+  defp package do
+    [# These are the default files included in the package
+     files: ["lib", "mix.exs", "README*", "README*", "LICENSE*"],
+     maintainers: ["Tim Smith"],
+     licenses: ["Apache 2.0"],
+     links: %{"GitHub" => "https://github.com/tesseract-tech/velkoz",
+              "Docs" => "https://hexdocs.pm/velkoz/0.0.1"}]
+  end
+
 end
