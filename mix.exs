d8da8a314c77586b96a719ac4c060f54c02d65cc
Tim Smith <tim.smith@sparksgrove.com>
2016-05-08 11:21:05 -0400

    updated version number

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Velkoz.Mixfile do
 
   def project do
     [app: :velkoz,
-     version: "0.0.1",
+     version: "1.0.0",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
